cceeeebc87a50fda1de6f1f58c8a1831ebe92d1e
mlearning23 <158996459+mlearning23@users.noreply.github.com>
2024-02-07 12:56:22 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
 
-
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -16,7 +15,6 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
-
 
 
 
